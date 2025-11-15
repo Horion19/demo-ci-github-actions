e83f67270d3e15afcd43988735b89c7de5fa0956
Horion19 <161644612+Horion19@users.noreply.github.com>
2025-11-14 20:10:07 -0600

    a ver si ya

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,10 @@
-// Crea tests/suma.test.js
-const assert = require('assert');
-const { suma, multiplica } = require('./index');
+﻿const assert = require('assert');
+const { suma, multiplica } = require('../index');
 
 try {
-    // Pruebas de suma
     assert.strictEqual(suma(2, 3), 5);
     assert.strictEqual(suma(-1, 1), 0);
     assert.strictEqual(suma(0, 0), 0);
-    
-    // Pruebas de multiplicación
     assert.strictEqual(multiplica(2, 3), 6);
     assert.strictEqual(multiplica(-1, 1), -1);
     assert.strictEqual(multiplica(0, 5), 0);
